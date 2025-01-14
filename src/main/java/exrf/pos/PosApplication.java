0016aefe936adbc8f5747ee0467925f216116c56
abd-saad <4bd.saad1999@gmail.com>
2025-01-14 14:07:41 +0700

    Remove unnecessary void

--- src/main/java/exrf/pos/PosApplication.java
+++ src/main/java/exrf/pos/PosApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class PosApplication {
 
 	public static void main(String[] args) {
