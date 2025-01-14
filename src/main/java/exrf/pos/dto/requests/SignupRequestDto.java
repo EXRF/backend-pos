dc759a889a735d3065a7a84b8c6221d22475dd4e
abd-saad <4bd.saad1999@gmail.com>
2025-01-14 16:53:23 +0700

    Add global exception handler, add validation on input

--- src/main/java/exrf/pos/dto/requests/SignupRequestDto.java
+++ src/main/java/exrf/pos/dto/requests/SignupRequestDto.java
@@ -5,19 +5,22 @@ import java.util.Set;
 import jakarta.validation.constraints.*;
 
 public class SignupRequestDto {
-    @NotBlank
-    @Size(min = 3, max = 20)
+    @NotBlank(message = "Username must not be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
 
-    @NotBlank
-    @Size(max = 50)
-    @Email
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Invalid email format")
     private String email;
 
     private Set<String> role;
 
-    @NotBlank
-    @Size(min = 6, max = 40)
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 6, max = 40, message = "Password must be between 6 and 40 characters")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]{6,40}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
+    )
     private String password;
 
     public String getUsername() {
