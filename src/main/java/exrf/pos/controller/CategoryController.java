767cfffd6f6c34602c40d55e29d9eddfe2778750
abd-saad <4bd.saad1999@gmail.com>
2025-01-17 10:05:48 +0700

    Add get category by id

--- src/main/java/exrf/pos/controller/CategoryController.java
+++ src/main/java/exrf/pos/controller/CategoryController.java
@@ -1,19 +1,33 @@
 package exrf.pos.controller;
 
-import exrf.pos.dto.requests.category.CreateCategoryRequestDto;
-import exrf.pos.repository.CategoryRepository;
-import exrf.pos.security.JwtUtils;
+import exrf.pos.exception.ResourceNotFoundException;
+import exrf.pos.model.Category;
+import exrf.pos.service.CategoryService;
+import exrf.pos.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/category")
 public class CategoryController {
     @Autowired
-    CategoryRepository categoryRepository;
+    CategoryService categoryService;
 
-    @Autowired
-    JwtUtils jwtUtils;
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOne(@PathVariable("id") Long id) {
+        Optional<Category> optCategory = categoryService.getOne(id);
+        if (optCategory.isEmpty()) {
+            throw new ResourceNotFoundException("Not Found");
+        }
+
+        Category category = new Category();
+
+        return ResponseEntity.ok(
+                ResponseUtil.responseSuccess(Category.class, category, "Success")
+        );
+    }
 
 }
--- src/main/java/exrf/pos/service/CategoryService.java
+++ src/main/java/exrf/pos/service/CategoryService.java
@@ -1,11 +1,18 @@
 package exrf.pos.service;
 
+import exrf.pos.model.Category;
 import exrf.pos.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class CategoryServiceImpl {
+public class CategoryService {
     @Autowired
     CategoryRepository categoryRepository;
+
+    public Optional<Category> getOne(Long id) {
+        return categoryRepository.findById(id);
+    }
 }
